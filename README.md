efd691a874d8ad7ed35e976a794bae99050c041a
Felix Meyer-Wolters <felix@egoversum.com>
2017-08-07 23:05:58 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+```
+Inject Help Text here.
+```
+
+# cmdinter
+
+<small>Project created on: 2017-08-06 21:16:57
+
+### Description
+
+A "command interface" for functions.
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from codecs import open
+from buildlib.yaml import load_yaml
+
+with open('README.md') as f:
+    long_description = f.read()
+
+config = load_yaml('CONFIG.yaml')
+
+setup(
+    name=config['public_name'],
+    version=config['version'],
+    author=config['author'],
+    author_email=config['author_email'],
+    maintainer=config['maintainer'],
+    maintainer_email=config['maintainer_email'],
+    url=config['url'],
+    description=config['description'],
+    long_description=long_description,
+    download_url=config['url'] + '/tarball/' + config['version'],
+    license=config['license'],
+    keywords=config['keywords'],
+
+    include_package_data=True,
+    platforms=config['pypi']['platforms'],
+    classifiers=config['pypi']['classifiers'],
+    install_requires=config['pypi']['install_requires'],
+    packages=find_packages(where='.',
+                           exclude=('tests', 'tests.*', 'venv-cmdinter', 'venv-cmdinter.*')),
+    package_dir=config['pypi']['package_dir'],
+    package_data=config['pypi']['package_data'],
+    data_files=config['pypi']['data_files'],
+    entry_points=config['pypi']['entry_points'],
+    tests_require=config['pypi']['tests_require']
+    )
